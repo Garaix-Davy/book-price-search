8be6da17453a6b7b67e2e500339ce895b3c7a7cb
Garaix-Davy <dgaraix@hotmail.com>
2018-04-03 23:59:53 -0600

    1

--- js/functions.js
+++ js/functions.js
@@ -152,13 +152,15 @@ function start() {
             App.lastResult = code;
 
             var isbn = document.getElementById("isbn");
-            var readings = document.getElementById("readings");
-            isbn.value = code;
-            readings.innerHTML += "<li>" + code + "</li>";
+            isbn.value += code + "\n";
 
 
-
-
+            // var $node = null, canvas = Quagga.canvas.dom.image;
+            //
+            // $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            // $node.find("img").attr("src", canvas.toDataURL());
+            // $node.find("h4.code").html(code);
+            // $("#result_strip ul.thumbnails").prepend($node);
         }
     });
 }
