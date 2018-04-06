b58dfa22aff96500ca37b4b2c430bc60a2857bff
Garaix-Davy <dgaraix@hotmail.com>
2018-04-06 00:25:23 -0600

    version 1

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,8 @@
 var debugDiv = document.getElementById("debug");
 
-function scan(){
+function toggleScanner(){
   var scanButton = document.getElementById("scan");
   if (scanButton.getAttribute("clicked") === "false"){
-    console.log("here");
     scanButton.setAttribute("clicked","true");
     scanButton.innerHTML = "Stop Scanner";
     start();
@@ -11,9 +10,61 @@ function scan(){
     Quagga.stop();
     scanButton.setAttribute("clicked","false");
     scanButton.innerHTML = "Scan Barcode";
+    // searchEbay("9781595550781"); // for testing - remove this.
   }
 }
 
+function searchEbay(isbnValue){
+  var results = document.getElementById("results");
+  results.innerHTML = "";
+  var appID = "DavyGara-bookpric-PRD-a78731904-738f601d";
+  var url = 'http://svcs.ebay.com/services/search/FindingService/v1'
+                    +'?OPERATION-NAME=findCompletedItems'
+                    +'&SECURITY-APPNAME=' + appID
+                    +'&GLOBAL-ID=EBAY-US'
+                    +'&RESPONSE-DATA-FORMAT=JSON'
+                    +'&REST-PAYLOAD'
+                    +'&keywords=' + isbnValue
+                    +'&categoryId=267'
+                    +'&itemFilter(0).name=SoldItemsOnly'
+                    +'&itemFilter(0).value=true'
+                    +'&sortOrder=EndTimeSoonest'
+                    +'&paginationInput.entriesPerPage=10';
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url );
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var resp = JSON.parse(xhr.responseText);
+      var books = resp.findCompletedItemsResponse[0].searchResult[0].item || [];
+      var html = [];
+      if (books.length > 0){
+        html.push('<table id="bookTable">');
+        html.push('<tr><th>Book Title ('+isbnValue+')</th><th>Condition</th><th>Sold Price (shipping included)</th><th>Date Sold</th></tr>');
+        for (i = 0; i < books.length; i++) {
+            var bookTitle = books[i].title;
+            var bookCondition = books[i].condition[0].conditionDisplayName;
+            var bookShippingCost = 0;
+            if (books[i].hasOwnProperty('shippingInfo') && books[i].shippingInfo[0].hasOwnProperty('shippingServiceCost')){
+              bookShippingCost = parseFloat(books[i].shippingInfo[0].shippingServiceCost[0].__value__);
+            }
+            var bookSoldPrice = parseFloat(books[i].sellingStatus[0].currentPrice[0].__value__) + bookShippingCost;
+            var dateSold = books[i].listingInfo[0].endTime.toString().substring(0,10);
+            html.push('<tr><td>'+bookTitle+'</td><td>'+bookCondition+'</td><td>'+'$'+bookSoldPrice.toFixed(2)+'</td><td>'+dateSold+'</td></tr>');
+        }
+        html.push('</table>');
+        results.innerHTML = html.join("");
+      } else {
+        results.innerHTML = "Sorry, this book didn't sell on eBay (US) recently. Please scan another.";
+      }
+    } else {
+      results.innerHTML = "Error: " + xhr.status + "<br>" + url;
+    }
+  }
+}
+
+// The function below is a modified version of the barcode API for this app.
 function start() {
     var App = {
         init : function() {
@@ -71,31 +122,9 @@ function start() {
                 return;
             }
         },
-        // initCameraSelection: function(){
-        //     var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();
-        //
-        //     return Quagga.CameraAccess.enumerateVideoDevices()
-        //     .then(function(devices) {
-        //         function pruneText(text) {
-        //             return text.length > 30 ? text.substr(0, 30) : text;
-        //         }
-        //         var $deviceSelection = document.getElementById("deviceSelection");
-        //         while ($deviceSelection.firstChild) {
-        //             $deviceSelection.removeChild($deviceSelection.firstChild);
-        //         }
-        //         devices.forEach(function(device) {
-        //             var $option = document.createElement("option");
-        //             $option.value = device.deviceId || device.id;
-        //             $option.appendChild(document.createTextNode(pruneText(device.label || device.deviceId || device.id)));
-        //             $option.selected = streamLabel === device.label;
-        //             $deviceSelection.appendChild($option);
-        //         });
-        //     });
-        // },
         attachListeners: function() {
             var self = this;
 
-            // self.initCameraSelection();
             $(".controls").on("click", "button.stop", function(e) {
                 e.preventDefault();
                 Quagga.stop();
@@ -162,17 +191,12 @@ function start() {
 
         if (App.lastResult !== code) {
             App.lastResult = code;
-
             var isbn = document.getElementById("number");
             isbn.value = code;
 
+            toggleScanner();
+            searchEbay(isbn.value);
 
-            // var $node = null, canvas = Quagga.canvas.dom.image;
-            //
-            // $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-            // $node.find("img").attr("src", canvas.toDataURL());
-            // $node.find("h4.code").html(code);
-            // $("#result_strip ul.thumbnails").prepend($node);
         }
     });
 }
